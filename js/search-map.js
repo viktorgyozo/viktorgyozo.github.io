e8039fb2e8fbf3f75cbea2222b34f2879746975b
Viktor Horvath <viktorgyozo@t-online.hu>
2024-11-27 21:42:56 +0100

    fix

--- js/search-map.js
+++ js/search-map.js
@@ -9,12 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const markersLayer = L.layerGroup().addTo(map);
 	
     fetch('./fsz.geojson')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return fsz.json();
-    })
     .then(geojsonData => {
         fsz = geojsonData;
       console.log(fsz);
